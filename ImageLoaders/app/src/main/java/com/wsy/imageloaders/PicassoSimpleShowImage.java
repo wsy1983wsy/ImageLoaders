fc8a957d5eab102c9a6e8388594ec33f7d9dd218
songyewang <wsy1983wsy@163.com>
2016-09-21 15:27:34 +0800

    获取显示的图片的bitmap

--- ImageLoaders/app/src/main/java/com/wsy/imageloaders/PicassoSimpleShowImage.java
+++ ImageLoaders/app/src/main/java/com/wsy/imageloaders/PicassoSimpleShowImage.java
@@ -1,14 +1,19 @@
 package com.wsy.imageloaders;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Picasso.LoadedFrom;
+import com.squareup.picasso.Target;
 
 import java.io.File;
 
@@ -21,7 +26,6 @@ public class PicassoSimpleShowImage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simple_image);
         findViewById(R.id.animateButton).setVisibility(View.GONE);
-        findViewById(R.id.fadeInButton).setVisibility(View.GONE);
     }
 
     public void onSimpleImageClicked(View view) {
@@ -61,5 +65,25 @@ public class PicassoSimpleShowImage extends AppCompatActivity {
         ImageView targetImageView = (ImageView) findViewById(R.id.imageView);
         String imageUrl = Urls.ImageURls.get(0);
         Picasso.with(this).load(imageUrl).resize(100, 100).into(targetImageView);
+    }
+
+    public void onFadeInImageClicked(View view) {
+        String imageUrl = Urls.ImageURls.get(3);
+        Picasso.with(this).load(imageUrl).into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, LoadedFrom from) {
+                Log.d("Picasso", "" + bitmap.getWidth());
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        });
     }
 }
